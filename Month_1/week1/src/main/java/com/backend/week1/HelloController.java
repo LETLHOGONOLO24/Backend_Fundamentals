48d167588250b03b4d0ffefe201ea979d9e41614
LETLHOGONOLO24 <pitroschauke24@gmail.com>
2026-02-06 16:55:52 +0200

    Configurationm Control, and why Hardcoding is Dangerous

--- Month_1/week1/src/main/java/com/backend/week1/HelloController.java
+++ Month_1/week1/src/main/java/com/backend/week1/HelloController.java
@@ -2,14 +2,24 @@ package com.backend.week1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class HelloController {
 
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping("/hello")
     public String hello() throws InterruptedException {
         Thread.sleep(3000);
-        return "Completed";
+        return "Hello from port " + port;
     }
-    // http://localhost:8080/hello
+    // http://localhost:8081/hello
+
+    @GetMapping("/fast")
+    public String fast() {
+        return "Fast response from port " + port;
+    }
+
 }
